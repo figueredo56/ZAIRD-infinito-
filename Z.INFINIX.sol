b6de6848ec1bb3c9e3f24f45d7faaf894900c1f7
ZAARD <143908070+figueredo56@users.noreply.github.com>
2026-06-10 22:13:23 -0400

    Create ZInfinixToken contract with ERC20 features

---
+++ Z.INFINIX.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract ZInfinixToken {
+    string public name = "Z.INFINIX";
+    string public symbol = "Z.INFINIX";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 100 * 10**18;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= value, "Balance insuficiente");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool success) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool success) {
+        require(value <= balanceOf[from], "Balance insuficiente");
+        require(value <= allowance[from][msg.sender], "Sin autorizacion");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+}
